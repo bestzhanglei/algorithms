a18aa4486432ac82bb90d73ca838de02f8e94bef
zhangl <zhangl@drore.com>
2017-08-17 18:01:23 +0800

    补充内容

--- algorithm-basic/src/demo/BasicDemo.java
+++ algorithm-basic/src/demo/BasicDemo.java
@@ -27,7 +27,7 @@ public class BasicDemo {
 
     }
 
-    //欧几里德算法
+    //欧几里德算法：两个整数的最大公约数等于其中较小的那个数和两数相除余数的最大公约数
     public static int gcd(int p, int q) {
         if (q == 0) {
             return p;
--- algorithm-find/src/demo/BinarySearch.java
+++ algorithm-find/src/demo/BinarySearch.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 /**
  * Author: zhangl
  * Date: 2017/8/17
- * Description:
+ * Description: 二分查找算法
  * Project: algorithms
  */
 public class BinarySearch {
@@ -21,16 +21,20 @@ public class BinarySearch {
         for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
-
         //将数组排序为升序数字顺序
         Arrays.sort(arr);
-        System.out.println(find(key, arr));
 
-        //Arrays有二分查找方法
+        //基础算法输出
+        System.out.println(find(key, arr));
+        //递归实现输出
+        System.out.println(find1(key, arr));
+
+        //Arrays内二分查找方法
         System.out.println(Arrays.binarySearch(arr, key));
 
     }
 
+    //基础实现
     public static int find(int key, int[] a) {
         //必须是有序数组
         int start = 0;
@@ -43,6 +47,19 @@ public class BinarySearch {
             else return mid;
         }
         return -1;
+    }
+
+    //二分查找的递归实现
+    public static int find1(int key, int[] a) {
+        return find1(key, a, 0, a.length - 1);
+    }
+
+    public static int find1(int key, int[] a, int start, int end) {
+        if (start > end) return -1;
+        int mid = start + (start - end) / 2;
+        if (key < a[mid]) return find1(key, a, start, mid - 1);
+        else if (key > a[mid]) return find1(key, a, mid + 1, end);
+        else return mid;
     }
 
 }
